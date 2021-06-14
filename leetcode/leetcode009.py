d0ef434cbe2d17046e46eb81233da20fac9a2944
bingjunLiu <liubj07@163.com>
2021-06-14 22:57:13 +0800

    found a function

--- leetcode/leetcode009.py
+++ leetcode/leetcode009.py
@@ -1,2 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        int2list = list(str(x))
+        if int2list == int2list[::-1]:
+            return True
+        else:return False
